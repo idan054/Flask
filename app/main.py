05d2a3ce6a07e83f835fa19738d15d06487dd028
idan054 <idanbit80@gmail.com>
2023-12-22 22:14:59 +0200

    Heroku test5

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
-from api.addRow import addRow
-from api.woo_requests.product_details_by_name import product_details_by_name
+# from api.addRow import addRow
+# from api.woo_requests.product_details_by_name import product_details_by_name
 
 app = Flask(__name__)
 
@@ -10,14 +10,14 @@ def index():
     return "<h1>Welcome to Geeks for Geeks</h1>"
 
 
-@app.route('/success', methods=['POST', 'GET'])
-def success():
-    if request.method == 'POST':
-        main_field = request.form['main_field']
-        print(main_field)
-        data = product_details_by_name(main_field)
-        addRow(data)
-    return render_template('success.html')
+# @app.route('/success', methods=['POST', 'GET'])
+# def success():
+#     if request.method == 'POST':
+#         main_field = request.form['main_field']
+#         print(main_field)
+#         data = product_details_by_name(main_field)
+#         addRow(data)
+#     return render_template('success.html')
 
 # Debug only
 # app.run(host='0.0.0.0', port=91)
