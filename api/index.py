67a1e8e3d1275396ade59e976a708efe1d7a498c
idan054 <idanbit80@gmail.com>
2023-12-22 22:35:46 +0200

    vercel data based 1

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from api.addRow import addRow
 from api.woo_requests.product_details_by_name import product_details_by_name
+import os
 
 
 app = Flask(__name__)
@@ -9,6 +10,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/api')
+def api():
+    with open(
+        os.path.join(os.getcwd(), 'data', 'data.json'), mode='r'
+    ) as my_file:
+        text = my_file.read()
+        return text
 
 
 @app.route('/success', methods=['POST', 'GET'])
